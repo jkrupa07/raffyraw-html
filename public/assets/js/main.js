868e7ef8f37ebc40d1c403b7e8862f35b0dcb35b
isha-sondagar <ishasondagar2@gmail.com>
2026-01-29 10:01:35 +0530

    remainnnn

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -20419,13 +20419,32 @@ var App = /*#__PURE__*/function () {
   }, {
     key: "Workingslider",
     value: function Workingslider() {
-      $('.working-slick').slick({
-        dots: true,
-        infinite: true,
+      $('.slider-inner').slick({
+        dots: false,
+        infinite: false,
         speed: 300,
         slidesToShow: 3,
         slidesToScroll: 1,
-        autoplay: true
+        arrows: false,
+        responsive: [{
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1
+          }
+        }, {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        }, {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }]
       });
     }
   }]);
